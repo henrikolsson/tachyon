9b54181f9099c1e9c3678aa2d0b101acbefaea9e
Henrik Olsson <henrik@fixme.se>
2013-09-20 23:57:58 +0200

    Update dependency versions

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject tachyon "0.0.2-SNAPSHOT"
   :description "a clojure irc library"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.apache.mina/mina-core  "2.0.0-RC1"]
-                 [org.slf4j/slf4j-api "1.6.0"]
-                 [clj-stacktrace "0.2.0"]])
+                 [org.apache.mina/mina-core  "2.0.7"]
+                 [org.slf4j/slf4j-api "1.7.5"]
+                 [clj-stacktrace "0.2.6"]])
