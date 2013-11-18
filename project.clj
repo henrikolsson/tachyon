d36ab525fee5f0a27e3f443fafca59bf4f6f4314
Henrik Olsson <henrik@fixme.se>
2013-11-18 22:22:09 +0100

    Add metadata to project file

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject tachyon "0.0.2-SNAPSHOT"
   :description "a clojure irc library"
+  :url "https://github.com/henrikolsson/tachyon"
+  :license {:name "Apache license version 2"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.apache.mina/mina-core  "2.0.7"]
                  [org.slf4j/slf4j-api "1.7.5"]
