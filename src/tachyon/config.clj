af97a639e5fea6664a11ada22b8f3378300c7a3a
Henrik Olsson <henrik@fixme.se>
2015-02-23 18:21:24 +0100

    Remove unused files

--- src/tachyon/config.clj
+++
@@ -1,9 +0,0 @@
-(ns demogorgon.config)
-
-(def config
-     {:irc
-      {:nick "clojgorgon"
-       :username "clojgorgon"
-       :realname "clojgorgon"
-       :servers [["irc.freenode.net" 6667] ["kornbluth.freenode.net" 6667]]
-       :channels ["#dem0"]}})
